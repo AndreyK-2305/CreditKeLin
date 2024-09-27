9297dec6768ff5c55f69d2c9e0e2d26d6d8fa795
AndreyK-2305 <kevinandreyjc@ufps.edu.co>
2024-09-27 13:16:52 -0500

    feat: added Product to admin

--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
 
-from .models import ProductType
+from .models import ProductType, Product
 
 # Register your models here.
 @admin.register(ProductType)
 class ProductTypeAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'status']
     search_fields = ['name']
+
+@admin.register(Product)
+class PorductAdmin(admin.ModelAdmin):
+    list_display = ['Product_id', 'Product_name', 'price' , 'Available', 'Description']
+    search_fields = ['name', 'Description']
