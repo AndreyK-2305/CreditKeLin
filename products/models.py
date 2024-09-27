ee8a9813022cdfb0984d0f8b06d65a02855e46f8
AndreyK-2305 <kevinandreyjc@ufps.edu.co>
2024-09-27 11:53:16 -0500

    chore: created model Product, settings configure and migrations

--- products/models.py
+++ products/models.py
@@ -13,3 +13,16 @@ class ProductType(models.Model):
 
     def __str__(self) -> str:
         return self.name
+    
+#Product
+class Product(models.Model):
+    
+    Product_id = models.IntegerField(null=False)
+    Type_Product_Id = models.ForeignKey(ProductType, on_delete=models.CASCADE)
+    Price = models.DecimalField(null=False, max_digits=10, decimal_places=2)
+    Product_Name = models.CharField(null=False, max_length=30)
+    Description = models.CharField(max_length=100)
+    Available = models.IntegerField(null=False)
+
+    def __str__(self) -> str:
+        return self.name
