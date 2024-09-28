ce179d108b35d4a6183c3caf091e00a8b341f1c6
AndreyK-2305 <kevinandreyjaimes1@gmail.com>
2024-09-27 21:31:19 -0500

    feat: added credit and payment to admin

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 
-from .models import ProductType, Product, Payment
+from .models import ProductType, Product, Payment, Credit, User
 
 # Register your models here.
 @admin.register(ProductType)
@@ -15,6 +15,16 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ['name', 'Description']
 
 @admin.register(Payment)
-class Payment(admin.ModelAdmin):
+class PaymentAdmin(admin.ModelAdmin):
     list_display = ['Payment_id', 'Value', 'Payment_STATUS']
     search_fields = ['Payment_id']
+
+@admin.register(Credit)
+class CreditAdmin(admin.ModelAdmin):
+    list_display = ['Credit_id', 'Client_id', 'Status','Created_at', 'Product_id']
+    search_fields = ['Status',]
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['Client_id', 'name', 'username','type']
+    search_fields = ['name']
