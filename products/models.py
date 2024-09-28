b4622c10f23f9404f48860b7aad759c978c04731
AndreyK-2305 <kevinandreyjaimes1@gmail.com>
2024-09-27 21:22:48 -0500

    chore: Created model Credit, configure and migrations

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+import django.urls
+import django.utils.timezone
 # Create your models here.
 
 #ProductTypeModel
@@ -26,25 +27,17 @@ class Product(models.Model):
     def __str__(self) -> str:
         return self.name
     
-#Credit
-class Credit(models.Model):
-    
-    CREDITSTATUSES = {
-        "started": "Started",
-        "active": "Active",
-        "completed": "Completed",
-        "suspended": "Suspended"
-    }
-    Credit_id = models.IntegerField(null=False)
-    #Client_id = models.ForeignKey('User', on_delete=models.SET_NULL)
-    Status = models.CharField(choices=CREDITSTATUSES, max_length=10)
-    Created_at = models.DateField(null=False)
-    Product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-    #Payment_id = models.ForeignKey('Payment', on_delete=models.SET_NULL)
+#User
+class User(models.Model):
+    Client_id = models.IntegerField(null=False)
+    name = models.CharField(max_length=20, null=False)
+    cc = models.IntegerField()
+    username = models.CharField(max_length=20, null=False)
+    email = models.CharField(max_length=30, null=False)
+    password = models.CharField(max_length=30, null=False)
+    type = models.CharField(max_length=15)
 
-    def __str__(self) -> str:
-        return self.name
-    #Payment
+#Payment
 class Payment(models.Model):
 
     PAYMENTSTATUSES = {
@@ -52,11 +45,29 @@ class Payment(models.Model):
         "delayed": "Delayed",
         "completed": "Completed"
     }
-
     Payment_id = models.IntegerField(null=False)
     Payment_STATUS = models.CharField(max_length=10)
-    Value = models.DecimalField(null=False, max_digits=10, decimal_places=2)
-    Delayed_Value = models.DecimalField(null=False, max_digits=10, decimal_places=2)
+    Value = models.DecimalField(null=False, max_digits=10, decimal_places=2, default=0)
+    Delayed_Value = models.DecimalField(null=False, max_digits=10, decimal_places=2, default=0)
 
     def __str__(self) -> str:
         return self.name
+
+#Credit
+class Credit(models.Model):
+    CREDITSTATUSES = {
+        "started": "Started",
+        "active": "Active",
+        "completed": "Completed",
+        "suspended": "Suspended"
+    }
+    Credit_id = models.IntegerField(null=False)
+    Client_id = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
+    Status = models.CharField(choices=CREDITSTATUSES, max_length=10)
+    Created_at = models.DateField(null=False, default= django.utils.timezone.now)
+    Product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    Payment_id = models.ForeignKey('Payment', on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+    
