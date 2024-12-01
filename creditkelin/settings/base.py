0d752931f7147524bab442632c7904d7abcc58e4
AndreyK-2305 <kevinandreyjaimes1@gmail.com>
2024-12-01 18:26:39 -0500

    chore: link users login front to backend

--- creditkelin/settings/base.py
+++ creditkelin/settings/base.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'rest_framework_simplejwt',
+    'corsheaders',
 ]
 
 
@@ -54,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware', 
+    'django.middleware.common.CommonMiddleware',
 ]
 
 REST_FRAMEWORK = {
--- users/views.py
+++ users/views.py
@@ -1,17 +1,40 @@
-# Create your views here.
 from django.shortcuts import render
 from .models import User
 from rest_framework import viewsets
 from .serializers import UserSerializer
 from rest_framework.filters import OrderingFilter, SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+from rest_framework.permissions import AllowAny
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
-
     ordering_fields = ["name"]
     search_fields = ["name", "telefono", "cc"]
-    
+
+class LoginView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:  # Verifica si el usuario está activo
+                return Response({
+                    'message': 'Login successful',
+                    'user': {
+                        'id': user.id,
+                        'username': user.username,
+                        'email': user.email
+                    }
+                })
+            else:
+                return Response({"detail": "User account is disabled."}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
