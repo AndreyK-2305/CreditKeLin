3cc8d99c97f1fdf73255560c9b862d189f1286de
AndreyK-2305 <kevinandreyjaimes1@gmail.com>
2024-12-04 18:44:35 -0500

    fix: routea  in render deploy, create a superuser

--- create-super.py
+++ create-super.py
@@ -2,7 +2,7 @@ import os
 import django
 from django.contrib.auth import get_user_model
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'creditkelin.settings.prod')
 django.setup()
 
 User = get_user_model()
